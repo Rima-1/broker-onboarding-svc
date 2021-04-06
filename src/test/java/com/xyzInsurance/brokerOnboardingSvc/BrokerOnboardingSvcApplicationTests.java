80f73e13816469a74245f971ff2023ac5e695847
RimaPanja <rimapanja@gmail.com>
2021-04-06 21:35:50 +0530

    My Initial Commit

---
+++ src/test/java/com/xyzInsurance/brokerOnboardingSvc/BrokerOnboardingSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyzInsurance.brokerOnboardingSvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerOnboardingSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
